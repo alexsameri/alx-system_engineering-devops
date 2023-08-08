765ae0ce1e64146545867f0d30a177caa2732c6c
alexsameri <samerialexx@gmail.com>
2023-08-08 17:22:45 +0000

    API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
 
-import praw
+"""
+Queries the Reddit API and returns a list containing the titles of all hot
+articles for a given subreddit.
+"""
 
-reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
-                     client_secret='YOUR_CLIENT_SECRET',
-                     user_agent='YOUR_USER_AGENT')
+import requests
 
 
-def recurse(subreddit, hot_list=None):
-    if hot_list is None:
-        hot_list = []
-
-    try:
-        subreddit = reddit.subreddit(subreddit)
-        hot_articles = subreddit.hot(limit=None)
-
-        for article in hot_articles:
-            hot_list.append(article.title)
-
+def recurse(subreddit, hot_list=[], count=0, after=''):
+    """
+    Queries the Reddit API and returns a list containing the titles of all hot
+    articles for a given subreddit.
+    """
+    base = 'https://www.reddit.com/'
+    endpoint = 'r/{}/hot.json'.format(subreddit)
+    query_string = '?show="all"&limit=100&after={}&count={}'.format(
+        after, count)
+    url = base + endpoint + query_string
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 2)'}
+    response = requests.get(url, headers=headers)
+    if not response.ok:
         if len(hot_list) == 0:
             return None
-
-        if hot_articles.params.get('after'):
-            return recurse(subreddit, hot_list)
         else:
             return hot_list
 
-    except praw.exceptions.Redirect:
+    data = response.json()['data']
+    for post in data['children']:
+        hot_list.append(post['data']['title'])
+    after = data['after']
+    dist = data['dist']
+    if (after):
+        recurse(subreddit, hot_list, count + dist, after)
+
+    if len(hot_list) == 0:
         return None
+    else:
+        return hot_list
